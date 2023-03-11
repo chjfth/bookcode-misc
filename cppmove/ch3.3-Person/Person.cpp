a5e9801d866287dd2c1192d58f92d7a336c26cd3
Jimm Chen WDK10 <chjfth@gmail.com>
2023-03-11 09:43:17 +0800

    Add p50 example.
    Declared Copying Disables Moving (Fallback Enabled)

--- cppmove/ch3.3-Person/Person.cpp
+++ cppmove/ch3.3-Person/Person.cpp
@@ -26,11 +26,43 @@ namespace p50z
 
 		std::vector<Person> coll;
 		Person p { "Tina", "Fox" };
-		coll.push_back(p);			  // [C0] OK, copies p
+		coll.push_back(p);            // [C0] OK, copies p
 		coll.push_back(std::move(p)); // [D0] OK, moves p	
 	}
 
 } // namespace p50z
+
+namespace p50
+{
+	// The example from page 50.
+	// Declared Copying Disables Moving (Fallback Enabled)
+	
+	class Person
+	{
+	public:
+		std::string sur;
+		std::string giv;
+		Person(const char* s, const char* g) : sur{s}, giv{g} {}
+
+	public:
+		// copy constructor/assignment declared:
+		Person(const Person&) = default;
+		Person& operator=(const Person&) = default;
+		
+		// NO move constructor/assignment declared
+	};
+
+	void test_p50()
+	{
+		std::vector<Person> coll;
+
+		Person p{ "Tina", "fox" };
+		
+		coll.push_back(p);            // [C0] OK, copies p
+		coll.push_back(std::move(p)); // [D3] OK, copies p (as fallback)
+	}
+	
+} // namespace p50
 
 
 int main(int argc, char* argv[])
@@ -40,6 +72,8 @@ int main(int argc, char* argv[])
 	printf("Hello, cppmove CH3.3 Person!\n");
 
 	p50z::test_p50z();
+
+	p50::test_p50();
 
 	return 0;
 }
