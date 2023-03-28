d7f23f9b68350ea01213f27e6d11df9f590f976c
Jimm Chen WDK10 <chjfth@gmail.com>
2023-03-29 01:56:01 +0800

    p80_user_deleter

--- cppstdlib12/ch01to05/ch01to05.cpp
+++ cppstdlib12/ch01to05/ch01to05.cpp
@@ -87,10 +87,59 @@ namespace p77
 		
 		cout << "main_p79_delete_Nico_early() now return.\n";
 	}
-
 }
+
+namespace p80_user_deleter
+{
+	void main()
+	{
+		// two shared pointers representing two persons by their name
+		shared_ptr<string> pNico(new string("nico"), 
+			[](string* p)
+			{
+				cout << "Custom delete: " << *p << endl;
+			});
+		shared_ptr<string> pJutta(new string("jutta"));
+
+		// capitalize person names
+		(*pNico)[0] = 'N';
+		pJutta->replace(0, 1, "J");
+
+		// put them multiple times in a container
+		vector<shared_ptr<string>> whoMadeCoffee;
+		whoMadeCoffee.push_back(pJutta);
+		whoMadeCoffee.push_back(pJutta);
+		whoMadeCoffee.push_back(pNico);
+		whoMadeCoffee.push_back(pJutta);
+		whoMadeCoffee.push_back(pNico);
+
+		// print all elements
+		for (auto ptr : whoMadeCoffee) {
+			cout << *ptr << " ";
+		}
+		cout << endl;
+
+		// overwrite a name again
+		*pNico = "Nicolai";
+
+		// print all elements again
+		for (auto ptr : whoMadeCoffee) {
+			cout << *ptr << " ";
+		}
+		cout << endl;
+		// print some internal data
+		cout << "use_count: " << whoMadeCoffee[0].use_count() << endl;
+
+		// p80: Now retire pNico early.
+		pNico = nullptr;
+		whoMadeCoffee.resize(2);
+		
+		cout << "p80_user_deleter::main() now return.\n";
+	}
+}
+
 
 int main(int argc, char *argv[])
 {
-	p77::main_p79_delete_Nico_early();
+	p80_user_deleter::main();
 }
