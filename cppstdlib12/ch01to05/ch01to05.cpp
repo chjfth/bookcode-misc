3f8b7708678e654d30539f7f92630d76f79db2df
Jimm Chen WDK10 <chjfth@gmail.com>
2023-03-31 10:12:29 +0800

    p82: sharemem in a share_ptr

--- cppstdlib12/ch01to05/ch01to05.cpp
+++ cppstdlib12/ch01to05/ch01to05.cpp
@@ -203,9 +203,76 @@ namespace p81_mod1
 	}
 }
 
+#ifdef __linux__ // __USE_POSIX
+#include <memory> // for shared_ptr
+#include <sys/mman.h> // for shared memory (linux only)
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <cstring> // for strerror()
+#include <cerrno> // for errno
+#include <string>
+#include <iostream>
+
+namespace p82
+{
+	class SharedMemoryDetacher
+	{
+	public:
+		void operator () (int* p)
+		{
+			std::cout << "unlink /tmp1234" << std::endl;
+			if (shm_unlink("/tmp1234") != 0) {
+				std::cerr << "OOPS: shm_unlink() failed" << std::endl;
+			}
+		}
+	};
+	
+	std::shared_ptr<int> getSharedIntMemory (int num)
+	{
+		void* mem;
+		int shmfd = shm_open("/tmp1234", O_CREAT | O_RDWR, S_IRWXU | S_IRWXG);
+		if (shmfd < 0) {
+			throw std::string(strerror(errno));
+		}
+		if (ftruncate(shmfd, num * sizeof(int)) == -1) {
+			throw std::string(strerror(errno));
+		}
+		mem = mmap(nullptr, num * sizeof(int), PROT_READ | PROT_WRITE,
+			MAP_SHARED, shmfd, 0);
+		if (mem == MAP_FAILED) {
+			throw std::string(strerror(errno));
+		}
+		return std::shared_ptr<int>(static_cast<int*>(mem), SharedMemoryDetacher());
+	}
+	
+	void main()
+	{
+		// get and attach shared memory for 100 ints:
+		std::shared_ptr<int> smp(getSharedIntMemory(100));
+		
+		// init the shared memory
+		for (int i = 0; i < 100; ++i) {
+			smp.get()[i] = i * 42;
+		}
+
+		// deal with shared memory somewhere else
+		// ...
+
+		std::cout << "<return>" << std::endl;
+
+		// release shared memory here:
+		smp.reset();
+	}
+}
+
+#endif // __USE_POSIX
+
 
 int main(int argc, char *argv[])
 {
-	p80_user_deleter::main();
-	p81_mod1::main(argv[0]);
+	p82::main();
+	
+//	p80_user_deleter::main();
+//	p81_mod1::main(argv[0]);
 }
