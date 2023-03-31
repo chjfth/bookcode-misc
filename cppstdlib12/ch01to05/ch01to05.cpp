5d4fa286f798907c111a5696cba29a0182501f0c
Jimm Chen WDK10 <chjfth@gmail.com>
2023-03-31 14:29:09 +0800

    p85_circref() memory leak demo

--- cppstdlib12/ch01to05/ch01to05.cpp
+++ cppstdlib12/ch01to05/ch01to05.cpp
@@ -282,10 +282,58 @@ namespace p82
 
 #endif // __USE_POSIX
 
+namespace p85_circref
+{
+	class Person {
+	public:
+		string name;
+		shared_ptr<Person> mother;
+		shared_ptr<Person> father;
+		vector<shared_ptr<Person>> kids;
+
+	Person (const string& n,
+			shared_ptr<Person> m = nullptr,
+			shared_ptr<Person> f = nullptr)
+			: name(n), mother(m), father(f) {
+		}
+		~Person() {
+			// MEMORY LEAK !!! 
+			// The dtor never has a chance to be called in this program.
+			cout << "delete " << name << endl;
+		}
+	};
+
+	shared_ptr<Person> initFamily (const string& name)
+	{
+		shared_ptr<Person> mom(new Person(name + "’s mom"));
+		shared_ptr<Person> dad(new Person(name + "’s dad"));
+		shared_ptr<Person> kid(new Person(name, mom, dad));
+		mom->kids.push_back(kid);
+		dad->kids.push_back(kid);
+		return kid;
+	}
+
+	void main()
+	{
+		shared_ptr<Person> p = initFamily("nico");
+		cout << "nico's family exists" << endl;
+		cout << "- nico is shared " << p.use_count() << " times" << endl;
+		cout << "- name of 1st kid of nico's mom: "
+			<< p->mother->kids[0]->name << endl;
+
+		p = initFamily("jim");
+		cout << "jim's family exists" << endl;
+	}
+}
+
 
 int main(int argc, char *argv[])
 {
-	p81_mod1::test_reset(argv[0]);
+	p85_circref::main();
+	
+//	p77::main();
+	
+//	p81_mod1::test_reset(argv[0]);
 	
 //	p80_user_deleter::main();
 //	p81_mod1::main(argv[0]);
