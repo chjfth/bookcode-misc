46a1d3934650368ee1482f53d3154eefeaa52d07
chjvm-multiVS <chjfth@gmail.com>
2023-09-04 21:52:54 +0800

    Update deadloop_kernel.cu, now compare with null-work function.

--- cuda-by-example-2010/chapter03/deadloop_kernel.cu
+++ cuda-by-example-2010/chapter03/deadloop_kernel.cu
@@ -17,9 +17,11 @@
 #include "../common/book.h"
 #include "../common/chjdbg.h"
 
-__global__ void kernel( void ) 
+__global__ void kernel_null_work( int n ) {}
+
+__global__ void kernel_dead_loop( int n ) 
 {
-	int temp = 0;
+	int temp = n;
 	while(1)
 		temp++;
 }
@@ -28,11 +30,22 @@ int main( void )
 {
 	printf( "Kernel dead loop start.\n" );
 
-	uint64 usec_start = ps_GetOsMicrosecs64();
-	kernel<<<1,1>>>();
-	uint64 usec_end = ps_GetOsMicrosecs64();
+	uint64 usec0 = ps_GetOsMicrosecs64();
 
-	printf( "Kernel dead loop end. (%s millisec)\n", 
-		us_to_msecstring(usec_end - usec_start));
+	kernel_null_work<<<1,1>>>(1);
+
+	uint64 usec1 = ps_GetOsMicrosecs64();
+
+	kernel_null_work<<<1,1>>>(2);
+
+	uint64 usec2 = ps_GetOsMicrosecs64();
+
+	kernel_dead_loop<<<1,1>>>(0);
+
+	uint64 usec3 = ps_GetOsMicrosecs64();
+
+	printf( "kernel_null_work(1) : %s millisec)\n", us_to_msecstring(usec1-usec0) );
+	printf( "kernel_null_work(2) : %s millisec)\n", us_to_msecstring(usec2-usec1) );
+	printf( "kernel_dead_loop(0) : %s millisec)\n", us_to_msecstring(usec3-usec2) );
     return 0;
 }
