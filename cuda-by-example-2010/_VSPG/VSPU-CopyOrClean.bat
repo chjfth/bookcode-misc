c3f2d7a427312bbcfc9cfcc6fbae52c13e71715f
chjvm-multiVS <chjfth@gmail.com>
2023-09-16 11:11:06 +0800

    cuda-by-example: Now use VSPG v600.

---
+++ cuda-by-example-2010/_VSPG/VSPU-CopyOrClean.bat
@@ -0,0 +1,79 @@
+@echo off
+setlocal EnableDelayedExpansion
+set batfilenam=%~n0%~x0
+set batdir=%~dp0
+set batdir=%batdir:~0,-1%
+set _vspgINDENTS=%_vspgINDENTS%.
+call :Echos START from %batdir%
+
+if "%PlatformName%" == "Win32" (
+	set GLUT_DLL_FILENAM=glut32.dll
+) else if "%PlatformName%" == "x64" (
+	set GLUT_DLL_FILENAM=glut64.dll
+) else (
+	call :Echos 'PlatformName' unknown value, cannot determine GLUT_DLL_FILENAM.
+	exit /b 4
+)
+
+if not defined ExeDllDir (
+	call :Echos VSPG internal bug! ExeDllDir not defined.
+	exit /b 4
+)
+
+call :GetAbsPath dirBookRoot "%batdir%\.."
+
+REM ==== copy glut32.dll or glut64.dll to output exe's directory ====
+call "%_vspg_bootsdir%\CopyFilePatterns.bat" "%dirBookRoot%\bin" "%ExeDllDir%" "%GLUT_DLL_FILENAM%"
+
+exit /b %ERRORLEVEL%
+
+
+
+REM =============================
+REM ====== Functions Below ======
+REM =============================
+
+
+:Echos
+  REM This function preserves %ERRORLEVEL% for the caller,
+  REM and, LastError does NOT pollute the caller.
+  setlocal & set LastError=%ERRORLEVEL%
+  echo %_vspgINDENTS%[%batfilenam%] %*
+exit /b %LastError%
+
+:EchoAndExec
+  echo %_vspgINDENTS%[%batfilenam%] EXEC: %*
+  call %*
+exit /b %ERRORLEVEL%
+
+:EchoVar
+  setlocal & set Varname=%~1
+  call echo %_vspgINDENTS%[%batfilenam%]%~2 %Varname% = %%%Varname%%%
+exit /b 0
+
+:SetErrorlevel
+  REM Usage example:
+  REM call :SetErrorlevel 4
+exit /b %1
+
+:GetAbsPath
+  REM Get absolute-path from a relative-path(relative to %CD%). 
+  REM If already absolute, return as is.
+  REM If your input dir is relative to current .bat, use RelaPathToAbs instead.
+  REM Param1: Var name to receive output.
+  REM Param2: The input path.
+  REM
+  REM Feature 1: this function removes redundant . and .. from input path.
+  REM I
+  REM For example:
+  REM     call :GetAbsPath outvar C:\abc\.\def\..\123
+  REM Returns outvar:
+  REM     C:\abc\123
+  REM
+  REM Feature 2: It's just string manipulation, so the input path doesn't have to 
+  REM actually exist on the file-system.
+  REM
+  if "%~1"=="" exit /b 4
+  if "%~2"=="" exit /b 4
+  for %%a in ("%~2") do set "%~1=%%~fa"
+exit /b 0
