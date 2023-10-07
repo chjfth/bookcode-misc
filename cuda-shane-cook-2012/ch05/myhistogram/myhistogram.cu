chjfth/bookcode-misc

commit 70eb0a25abadd022886c3a90b83d18175bb26193

[CUDA2012] myhistogram version 20231003.1: Use dim3 for num_blocks, now we can run 65536+ num_blocks.

cuda-shane-cook-2012/ch05/myhistogram/myhistogram.cu
@@ -10,7 +10,7 @@ quite appositely matches the author's words.
 #include "../../share/share.h"
 #include "mykernels.h"
 
-const char *g_version = "20231002.2";
+const char *g_version = "20231003.1";
 
 void ReportErrorIfNot4xSamples(const char *title, int sample_count)
 {
@@ -85,6 +85,8 @@ bool generate_histogram_gpu(const char *title, int sample_count, int threads_per
 	}
 	assert(num_blocks>0);
 
+	dim3 griddim = my_blocks_to_dim3(num_blocks);
+
 	printf("[%s] Counting %d samples (%d blocks * %d threads) ...\n", title, 
 		sample_count, num_blocks, threads_per_block);
 
@@ -107,29 +109,29 @@ bool generate_histogram_gpu(const char *title, int sample_count, int threads_per
 
 	if(strcmp(title, "p98:myhistogram_01")==0)
 	{
-		myhistogram_01<<<num_blocks, threads_per_block>>>
+		myhistogram_01<<<griddim, threads_per_block>>>
 			(kaSamples, kaCount, sample_count);
 	}
 	else if(strcmp(title, "p99:myhistogram_02")==0)
 	{
-		myhistogram_02<<<num_blocks, threads_per_block>>>
+		myhistogram_02<<<griddim, threads_per_block>>>
 			((Uint*)kaSamples, kaCount, sample_ints);
 	}
 	else if(strcmp(title, "myhistogram_03b")==0)
 	{
-		myhistogram_03b<<<num_blocks, threads_per_block>>>
+		myhistogram_03b<<<griddim, threads_per_block>>>
 			(kaSamples, kaCount, sample_count);
 	}
 	else if(strcmp(title, "p101:myhistogram_03a")==0)
 	{
-		myhistogram_03a<<<num_blocks, threads_per_block>>>
+		myhistogram_03a<<<griddim, threads_per_block>>>
 			((Uint*)kaSamples, kaCount, sample_ints);
 	}
 	else if(strcmp(title, "p102:myhistogram_07")==0)
 	{
 		printf("Using Nbatch = %d\n", Nbatch);
 
-		myhistogram_07<<<num_blocks, threads_per_block>>>
+		myhistogram_07<<<griddim, threads_per_block>>>
 			((Uint*)kaSamples, kaCount, sample_ints, Nbatch);
 	}
 	else
@@ -195,9 +197,10 @@ bool generate_histogram_gpu(const char *title, int sample_count, int threads_per
 extern"C" void 
 main_myhistogram(int argc, char* argv[])
 {
+	printf("myhistogram version %s\n", g_version);
+
 	if(argc==1)
 	{
-		printf("myhistogram.exe version %s\n", g_version);
 		printf("Usage:\n");
 		printf("    myhistogram <histogram_sample_count> [threads_per_block] [Nbatch]\n");
 		printf("\n");

cuda-shane-cook-2012/share/share.h
@@ -98,6 +98,36 @@ static void HandleError( cudaError_t err,
 
 void myPrintGpuInfo();
 
+inline dim3 my_blocks_to_dim3(int num_blocks)
+{
+	const int WIDTHX = 16384;
+	const int WIDTHY = 16384;
+	dim3 d(1, 1, 1);
+
+	if(num_blocks<=WIDTHX)
+	{
+		d.x = num_blocks;
+	}
+	else
+	{
+		d.x = WIDTHX;
+
+		int yz = OCC_DIVIDE(num_blocks, WIDTHX);
+
+		if(yz<=WIDTHY)
+		{
+			d.y = yz;
+		}
+		else
+		{
+			d.y = OCC_DIVIDE(yz, WIDTHY);
+		}
+	}
+
+	return d;
+}
+
+
 }; // extern"C"
 
 #endif

commit 886336ff60e4e04f515ea7259519c1fdc9c7fda3

myhistogram_07.cu : Remove wrong assert().
This error was introduced since 70eb0a25. Since then, gridDim.y can be larger than 1.

TO reproduce, run (used Debug-build to make assert() effective):

   D:\barn\x64\__Debug\myhistogram.exe 65540 1 1

Note:

   myhistogram.exe 65536 1 1

does not trigger that bug yet.

cuda-shane-cook-2012/ch05/myhistogram/myhistogram.cu
@@ -10,7 +10,7 @@ quite appositely matches the author's words.
 #include "../../share/share.h"
 #include "mykernels.h"
 
-const char *g_version = "20231007.1";
+const char *g_version = "20231007.2";
 
 void ReportErrorIfNot4xSamples(const char *title, int sample_count)
 {

cuda-shane-cook-2012/ch05/myhistogram/myhistogram_07.cu
@@ -8,12 +8,11 @@ __global__ void myhistogram_07( // @page 102
 {
 	// Chj: Note: this program still implies blockDim.y==1
 
-	assert(blockDim.y==1 && gridDim.y==1);
+	assert(blockDim.y==1);
 
 	/* Work out our thread id */
 	Uint idxN = (blockIdx.x * (blockDim.x*Nbatch)) + threadIdx.x;
 	Uint idyN = (blockIdx.y * blockDim.y) + threadIdx.y;
-	assert(idyN==0);
 	Uint tidN = idxN + idyN * (blockDim.x*Nbatch) * gridDim.x;
 
 	Uint threads_per_block = blockDim.x * blockDim.y;