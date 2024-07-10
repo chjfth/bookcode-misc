06bbf18da5aae4e39d64ef60b22428671a1cc42b
chj on Ub20cuda VM <chjfth@gmail.com>
2024-07-10 20:00:39 +0800

    myhistogram: add linux compile command

---
+++ cuda-shane-cook-2012/share/share_utils_linux.cpp
@@ -0,0 +1,90 @@
+#include <unistd.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdio.h>
+#include <pthread.h>
+
+#include "share.h"
+
+
+uint64 ps_GetOsMicrosecs64(void)
+{
+	struct timespec ts;
+	if(clock_gettime(CLOCK_MONOTONIC, &ts) != 0) {
+		return 0; // 0 means fail!
+	}
+	
+	uint64 ret = ts.tv_sec*1000*1000 + ts.tv_nsec/1000;
+	return ret==0 ? 1 : ret;
+}
+
+
+struct SwThreadParam
+{
+	PROC_ggt_simple_thread proc;
+	void *param;
+	pthread_t th; // th: thread handle
+	
+	SwThreadParam(){ memset(this, 0, sizeof(this)); }
+};
+
+
+static void *
+_LinuxThreadWrapper(void * param)
+{
+	SwThreadParam *pw = (SwThreadParam*)param;
+	pw->proc(pw->param);
+	return 0;
+}
+
+
+GGT_HSimpleThread 
+ggt_simple_thread_create(PROC_ggt_simple_thread proc, void *param, int stack_size)
+{
+	SwThreadParam *pwp = new SwThreadParam;
+	pwp->proc = proc;
+	pwp->param = param;
+	
+	int err = 0;
+	pthread_attr_t attr;
+	
+	if(stack_size>0)
+	{
+		pthread_attr_init(&attr);
+		
+		size_t stksize = stack_size;
+		pthread_attr_setstacksize(&attr, stksize);
+
+		err = pthread_create(&pwp->th, &attr, _LinuxThreadWrapper, pwp);
+
+		pthread_attr_destroy(&attr);
+	}
+	else
+	{
+		err = pthread_create(&pwp->th, NULL, _LinuxThreadWrapper, pwp);
+	}
+
+	if(!err)
+		return (GGT_HSimpleThread)pwp;
+	else
+		return NULL;
+}
+
+bool 
+ggt_simple_thread_waitend(GGT_HSimpleThread h)
+{
+	if(!h) {
+		return false;
+	}
+	
+	SwThreadParam *pwp = (SwThreadParam*)h;
+	
+	int err = pthread_join(pwp->th, NULL);
+	if(!err)
+	{
+		delete pwp;
+		return true;
+	}
+	else 
+		return false;
+}
