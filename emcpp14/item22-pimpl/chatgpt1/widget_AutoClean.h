ddd017d4c664a83bf38675dde14bdc8dacf73547
chj Win7Evernote VM <chjfth@gmail.com>
2026-05-27 18:25:00 +0800

    Source to ask ChatGPT.
    https://chatgpt.com/share/6a16acac-6334-83e8-8997-74771d1a9dd3

---
+++ emcpp14/item22-pimpl/chatgpt1/widget_AutoClean.h
@@ -0,0 +1,43 @@
+template<typename T>
+class AutoClean
+{
+private:
+    T *m_t;
+public:
+    AutoClean(T *pt) : m_t(pt) {}
+    ~AutoClean()
+    {
+        delete m_t;
+    }
+
+    operator bool() { return m_t != nullptr; }
+
+    operator T*() { return m_t; }
+    operator const T*() const { return m_t; }
+};
+
+class Widget
+{
+public:
+    Widget();
+    ~Widget();
+
+    // move-ctor/assign
+    Widget(Widget&& rhs);
+    Widget& operator=(Widget&& rhs);
+
+    // copy-ctor/assign
+    Widget(const Widget& rhs);
+    Widget& operator=(const Widget& rhs);
+
+    static void test_incomplete_type()
+    {
+        AutoClean<Impl> aci(nullptr); // wacky-test
+    }
+
+private:
+    struct Impl;
+
+    AutoClean<Impl> pImpl;
+    // std::unique_ptr<Impl> pImpl;
+};
